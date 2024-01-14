4fa27af0230af3cd0d6f5778a98264c1452ef0aa
Mykyta Tishkin <87891550+mykytatishkin@users.noreply.github.com>
2024-01-14 21:24:03 +0200

    code fix

--- index.js
+++ index.js
@@ -9,9 +9,20 @@ function updateBValue(inputElement) {
     const fixedMultiplier = 2; // Замените на ваше фиксированное число
 
     if (!isNaN(inputValue)) {
-        const bElement = inputElement.nextElementSibling.querySelector('b');
+        const bElement = findBElement(inputElement);
         if (bElement) {
             bElement.textContent = inputValue * fixedMultiplier;
         }
     }
+}
+
+function findBElement(inputElement) {
+    let currentElement = inputElement.parentElement; // Начинаем с родительского элемента
+    while (currentElement) {
+        if (currentElement.tagName === 'TD') {
+            return currentElement.querySelector('b');
+        }
+        currentElement = currentElement.parentElement;
+    }
+    return null; // Если не найден элемент <b>
 }
